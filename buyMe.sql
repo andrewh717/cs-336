961847f584e8341160c1376dc48074ddf31daf4b
Roslan24 <a.roslan24@gmail.com>
2018-04-11 20:26:39 -0400

    Final draft for the DB and Triggers

--- buyMe.sql
+++ buyMe.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS buyMe;
 CREATE DATABASE IF NOT EXISTS buyMe;
 USE buyMe;
 
+DROP TABLE IF EXISTS Account;
 CREATE TABLE Account(
 	username VARCHAR(50),
     password VARCHAR(128) BINARY NOT NULL,
@@ -13,6 +14,7 @@ CREATE TABLE Account(
     PRIMARY KEY (username)
 );
 
+DROP TABLE IF EXISTS Product;
 CREATE TABLE Product(
 	productId INT AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
@@ -25,9 +27,14 @@ CREATE TABLE Product(
 	seller VARCHAR(50) NOT NULL,
 	price DECIMAL(20,2) NOT NULL,
 	sold BOOLEAN,
+    startDate DATETIME,
+    endDate DATETIME,
+    FOREIGN KEY (seller) REFERENCES Account(username)
+		ON DELETE CASCADE,
 	PRIMARY KEY(productId, seller)
 );
 
+DROP TABLE IF EXISTS Bid;
 CREATE TABLE Bid(
 	currentBid DECIMAL(20,2),
 	buyer VARCHAR(50),
@@ -39,16 +46,20 @@ CREATE TABLE Bid(
 	PRIMARY KEY (currentBid, buyer, productId)
 );
 
+DROP TABLE IF EXISTS BuyingHistory;
 CREATE TABLE BuyingHistory(
+	productId INT,
 	price DECIMAL(20,2) NOT NULL,
 	buyer VARCHAR(50),
-	productId INT,
 	date DATETIME,
-	FOREIGN KEY (price, buyer, productId) REFERENCES Bid(currentBid, buyer, productId)
+	FOREIGN KEY (productId) REFERENCES Product(productId)
+		ON DELETE CASCADE,
+	FOREIGN KEY (buyer) REFERENCES Account(username)
 		ON DELETE CASCADE,
 	PRIMARY KEY (buyer, productId)
 );
 
+DROP TABLE IF EXISTS SellingHistory;
 CREATE TABLE SellingHistory(
 	productId INT,
 	seller VARCHAR(50),
@@ -56,10 +67,10 @@ CREATE TABLE SellingHistory(
 	date DATETIME,
 	FOREIGN KEY (productId, seller) REFERENCES Product(productId, seller)
 		ON DELETE CASCADE,
-	FOREIGN KEY (price) REFERENCES Bid(currentBid),
 	PRIMARY KEY (seller, productId)
 );
 
+DROP TABLE IF EXISTS Email;
 CREATE TABLE Email(
 	messageId INT AUTO_INCREMENT,
 	to_username VARCHAR(50),
@@ -70,22 +81,7 @@ CREATE TABLE Email(
     PRIMARY KEY (messageId, to_username, from_username)
 );
 
-DROP TABLE IF EXISTS BadPrice;
-CREATE TABLE BadPrice(
-	productId INT,
-    name VARCHAR(50) NOT NULL,
-	category VARCHAR(50) NOT NULL,
-	brand VARCHAR(25),
-	gender VARCHAR(10) NOT NULL,
-    size INT,
-    model VARCHAR(50),
-    color VARCHAR(20),
-	seller VARCHAR(50) NOT NULL,
-	price DECIMAL(20,2) NOT NULL,
-    FOREIGN KEY (productId) REFERENCES Product(productId),
-	PRIMARY KEY(productId)
-);
-
+DROP TRIGGER IF EXISTS PriceCheck;
 DELIMITER $$
 	CREATE TRIGGER PriceCheck AFTER INSERT ON Product
 	FOR EACH ROW
@@ -93,22 +89,98 @@ DELIMITER $$
 	IF NEW.price<0
 	THEN
 		BEGIN
-			INSERT INTO BadPrice VALUES(NEW.productId,NEW.name,NEW.category,NEW.brand,NEW.gender,NEW.size,NEW.model,NEW.color,NEW.seller,NEW.price);
-			DELETE FROM Product P WHERE P.productId=NEW.productId;
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='Price cannot be negative';
+		END;
+	END IF; 
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS SoldItems;
+DELIMITER $$
+	CREATE TRIGGER SoldItems AFTER UPDATE ON Product
+    FOR EACH ROW 
+    BEGIN
+    IF NEW.sold=true
+    THEN
+		BEGIN
+			INSERT INTO BuyingHistory (price, buyer, productId, date)
+            SELECT B.currentBid, B.buyer, B.productId, NOW()
+            FROM Bid B
+            WHERE B.productId=NEW.productId;
+            
+            INSERT INTO SellingHistory (productId, seller, price, date)
+            SELECT P.productId, P.seller, P.price, NOW()
+            FROM Product P
+            WHERE P.productId=NEW.productId;
+            
+            DELETE FROM Bid WHERE productId=NEW.productId;
+		END;
+	END IF; 
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS Checking;
+DELIMITER $$
+	CREATE TRIGGER Checking BEFORE INSERT ON Product
+    FOR EACH ROW
+    BEGIN
+    IF (EXISTS(
+		SELECT P.productId
+		FROM Product P
+        WHERE P.productId=NEW.productId
+	))THEN
+		BEGIN
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT='ProductId already exists';
+		END;
+	END IF; 
+END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS UpdatingPrice;
+DELIMITER $$
+	CREATE TRIGGER UpdatingPrice AFTER UPDATE ON Bid
+    FOR EACH ROW
+    BEGIN
+    IF (NEW.currentBid>OLD.currentBid)
+    THEN 
+		BEGIN
+			UPDATE Product
+			SET price=NEW.currentBid
+			WHERE NEW.productId=productId;
 		END;
 	END IF;
-delimiter ;
+END $$
+DELIMITER ;
 
+# For testing purposes
+/*
+INSERT INTO Product VALUES(1,'testboot','shoe','ra','M',4,'ras','black','roslan',5,false,'2018-03-05 01:01:01','2018-03-05 01:05:01');
+INSERT INTO Bid VALUES(5,'test',1);
 
-#DROP TABLE Shoes;
-/*CREATE TABLE Shoes(
-	productId INT,
-	category VARCHAR(50) NOT NULL,
-    brand VARCHAR(50),
-    size INT,
-    model VARCHAR(50),
-    color VARCHAR(50),
-    FOREIGN KEY (productId) REFERENCES Product(productId)
-		ON DELETE CASCADE,
-	PRIMARY KEY(productId)
-);*/
+DELETE FROM Product WHERE productId=1;
+DELETE FROM SellingHistory WHERE productId=1 AND seller='roslan';
+DELETE FROM BuyingHistory WHERE productId=1 AND buyer='test';
+
+UPDATE Product
+SET sold=true
+WHERE productId=1;
+
+UPDATE Bid
+SET currentBid=6
+WHERE productId=1;
+
+SELECT *
+FROM Product;
+
+SELECT *
+FROM Bid;
+
+SELECT *
+FROM BuyingHistory;
+
+SELECT *
+FROM SellingHistory;
+
+SELECT *
+FROM Account;
+*/
